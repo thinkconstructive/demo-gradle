60919c2dba4c79ab74ababdfff749f9f5679372d
thinkconstructive <>
2023-09-09 16:09:36 +0530

    Java Spring Boot Project with Gradle build

---
+++ src/main/java/com/thinkconstructive/demogradle/EmployeeApi.java
@@ -0,0 +1,16 @@
+package com.thinkconstructive.demogradle;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeApi {
+
+    @GetMapping("/{empId}")
+    public String getEmployee(String empId)
+    {
+        return "Employee Found";
+    }
+}
---
+++ src/test/java/com/thinkconstructive/demogradle/DemoGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinkconstructive.demogradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
